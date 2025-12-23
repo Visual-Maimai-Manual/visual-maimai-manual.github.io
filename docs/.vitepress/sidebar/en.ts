d00fc63279b8d2dbb2891ca6c9ea87ad36ad7f27
fjcdhc <joeyep114514@outlook.com>
2025-12-23 22:12:39 +0800

    画大饼！

--- docs/.vitepress/sidebar/en.ts
+++ docs/.vitepress/sidebar/en.ts
@@ -5,7 +5,13 @@ export const enSidebar: DefaultTheme.Sidebar = [
     text: 'Introduction',
     items: [
       { text: 'What is Visual Maimai', link: '/en/intro/what-is-vm' },
-      { text: 'Download Visual Maimai', link: '/en/intro/download' }
+      { text: 'Download Visual Maimai', link: '/en/intro/download' },
+    ]
+    },
+    {
+     text: 'Get Started',
+     items:[
+        { text: 'About Visual Maimai`s GUI', link: '/en/guide/gui'}
     ]
   }
 ]
